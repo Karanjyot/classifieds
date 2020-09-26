648cc80b6026ce71fdae1f7e983e8a4749dc1d5f
karanjyot <karanjyot.thandi@gmail.com>
2020-09-26 18:18:23 -0400

    setup user schema, connect to db via mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,32 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+//set up express
 
 const app = express();
-
-var PORT = 5000 || process.env.PORT;
 
 //middleware to handle JSON/URLencoded data. Parse JSON (turn json into js object)
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
-app.get("/hello", (req,res)=>{
-    res.send("hello worlds")
-})
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>(
-    console.log(`server is running on port ${PORT}`)
-))
+//set up routes as middleware
+
+app.use("/users", require("./routes/userRouter"))
+
+//set up mongoose
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/classifieddb",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("connected to DB");
+  }
+);
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
--- client/src/components/Items/Items.js
+++ client/src/components/Items/Items.js
@@ -4,16 +4,19 @@ import "./Items.css";
 function Items() {
   const items = [
     {
+      id:1,
       title: "Used Car for sale",
       description: "I am selling a used 2003 Honda Civic. Works well.",
       price: "$1000",
     },
     {
+      id:2,
       title: "computer for sale",
       description: "Selling a brand new Dell computer",
       price: "$800",
     },
     {
+      id:3,
       title: "Used phone for sale",
       description: "I am selling my iphone 1",
       price: "$400",
@@ -23,8 +26,8 @@ function Items() {
 
   return (
     <div>
-      {posts.map((post) => (
-        <div>
+      {posts.map(post => (
+        <div key={post.id}>
           <h3>{post.title}</h3>
           <p>{post.description}</p>
           <p>{post.price}</p>
